b5cc806a63f4623aa883d1ce86398b409e0404dc
briandozie <doziebrian01@gmail.com>
2022-10-14 15:31:37 +0800

    done web server

--- WebServer/Properties/AssemblyInfo.cs
+++ WebServer/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("WebServer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WebServer")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2022")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ffc6a921-b796-457d-87b1-667ad15468a6")]
+[assembly: Guid("31a6cfe6-8df8-4c78-9228-403207677a2d")]
 
 // Version information for an assembly consists of the following four values:
 //
