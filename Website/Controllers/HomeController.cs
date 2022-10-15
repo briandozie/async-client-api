9aa1baaf4abcf832d9395bbfb4a4d0ae47609857
briandozie <doziebrian01@gmail.com>
2022-10-15 09:57:38 +0800

    started website

---
+++ Website/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+using WebServer.Models;
+
+namespace Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Home";
+
+            // get list of all clients
+            RestClient client = new RestClient("http://localhost:50968/");
+            RestRequest request = new RestRequest("api/clients", Method.Get);
+            RestResponse response = client.Execute(request);
+
+            // deserialize into list
+            List<Client> clients = JsonConvert.DeserializeObject<List<Client>>(response.Content);
+
+            return View(clients);
+        }
+    }
+}
