8b3695e7a7e0a9ae2697a9a145351cc07ee9aaf9
briandozie <doziebrian01@gmail.com>
2022-10-18 13:41:44 +0800

    remote server

---
+++ RemoteServer/RemoteServerImpl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteServer
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, UseSynchronizationContext = false,
+        InstanceContextMode = InstanceContextMode.Single)]
+    internal class RemoteServerImpl : RemoteServerInterface
+    {
+        private List<string> jobs;
+
+        public RemoteServerImpl()
+        {
+            jobs = new List<string>();
+        }
+
+        public string Download()
+        {
+            string job = jobs[0];
+            jobs.RemoveAt(0);
+            return job;
+        }
+
+        public void Upload(string job)
+        {
+            jobs.Add(job);
+        }
+
+        public bool JobAvailable()
+        {
+            if(jobs.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ RemoteServer/RemoteServerInterface.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteServer
+{
+    [ServiceContract]
+    public interface RemoteServerInterface
+    {
+        [OperationContract]
+        [FaultContract(typeof(ServerFault))]
+        string Download();
+
+        [OperationContract]
+        [FaultContract(typeof(ServerFault))]
+        void Upload(string job);
+
+        [OperationContract]
+        [FaultContract(typeof(ServerFault))]
+        bool JobAvailable();
+    }
+
+    [DataContract]
+    public class ServerFault
+    {
+        private string reason;
+
+        [DataMember]
+        public string Reason
+        {
+            get { return reason; }
+            set { reason = value; }
+        }
+    }
+}
